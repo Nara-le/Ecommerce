80489e17648c0c35a68980b3be670337e9e9eef3
Nara-le <lesovannara@gmail.com>
2025-08-28 11:20:21 +0700

    v2.1

--- project3/settings.py
+++ project3/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ns(@i_@_ia_5we7-%xh9&1yh*bhfp8z7k&t8b&**sqe^caz%v^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
